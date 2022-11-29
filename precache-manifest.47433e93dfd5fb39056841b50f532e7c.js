3c055121411b15d0fd861c83b21ee5b4d71e3834
RABSON-DAO <78108344+RABSON-DAO@users.noreply.github.com>
2022-11-29 12:15:23 +0300

    Add files via upload

---
+++ precache-manifest.47433e93dfd5fb39056841b50f532e7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad87708bbb3c9189e3031ee70d1bf19c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12554763585a158a0df",
+    "url": "/static/css/2.c1c5c199.chunk.css"
+  },
+  {
+    "revision": "ec498038ebabbe5c5642",
+    "url": "/static/css/main.2a97de24.chunk.css"
+  },
+  {
+    "revision": "c12554763585a158a0df",
+    "url": "/static/js/2.5d317672.chunk.js"
+  },
+  {
+    "revision": "f5811d2d1dfb1e373de4a2809bbc145b",
+    "url": "/static/js/2.5d317672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec498038ebabbe5c5642",
+    "url": "/static/js/main.3e26c037.chunk.js"
+  },
+  {
+    "revision": "5fb71e6afc6134ef87e4",
+    "url": "/static/js/runtime-main.24643165.js"
+  }
+]);
